16530ea3d0c44fb3fcb73c9a8fc15a0960256c15
Jose Garcia <joseeduardo.garcia@rockabox.com>
2014-07-31 15:47:28 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(
+    name='django_remote_field',
+    version='1.0.0',
+    author='rockabox',
+    author_email='tech@rockabox.com',
+    packages=['remotefields'],
+    include_package_data=True,
+    url='https://github.com/rockabox/django-remote-field',
+    license='MIT',
+    description='Django Remote Field',
+    classifiers=[
+        'Development Status :: 2 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[
+        'Django>=1.6.5',
+        'djangorestframework>=2.3.13'
+    ],
+)
