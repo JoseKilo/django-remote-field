6a3b31b755e46510d8d06eb2e9718e0cbdb7ccda
Jose Garcia <joseeduardo.garcia@rockabox.com>
2014-08-21 11:54:48 +0100

    Allow several nesting levels

--- tests/models.py
+++ tests/models.py
@@ -3,3 +3,8 @@ from django.db import models
 
 class ModelForTest(models.Model):
     thing_id = models.IntegerField()
+
+
+class ParentModelForTest(models.Model):
+    test_instance = models.ForeignKey(ModelForTest)
+    test_instances = models.ManyToManyField(ModelForTest, related_name='+')
